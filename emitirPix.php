33c20ec6467cc64137fc68cabfcca00ae17b1a6d
Guilherme Cota | Gerencianet <guilherme.cota@gerencianet.com.br>
2020-12-04 15:39:36 -0300

    master

--- emitirPix.php
+++ emitirPix.php
@@ -29,6 +29,7 @@ $cep = "35400000"; // [opcional] CEP da localidade onde é efetuada a transaçã
 $arquivo_config = file_get_contents("./config.json");
 $configuracoes = json_decode($arquivo_config, true);
 $ambiente = ($configuracoes["sandbox"] === true) ? "homologacao" : "producao"; // Define o ambiente de acordo com o sadbox em configurações
+$nomeRecebedor = $configuracoes["recebedor"];
 
 $arq_certificado = "./certificado/" . $configuracoes[$ambiente]["nome_certificado"]; // Local onde está salvo o certificado .pem
 
@@ -41,21 +42,21 @@ $txID = getTxID($tipo); // Função gera um txID aleatório obedecendo a regex d
 $pix_url_cob = $configuracoes[$ambiente]["pix_url_cob"] . "/" . $txID; // Monta a url para a requisição que gera a cobrança
 
 // Insira as informações da cobrança
-$body = json_encode([
+$body = [
   "calendario" => [
     "expiracao" => 3600 // [opcional] Tempo de vida da cobrança, especificado em segundos a partir da data de criação. Caso não definido o padrão será de 86400 segundos ( 24 horas)
   ],
   "devedor" => [
     "cpf" => "12345678900", // [opcional] CPF do usuário pagador.string /^\d{11}$/
     //"cnpj" => "12345678000123",
-    "nome" => "Nome Aqui" // [opcional] Nome do usuário pagador. string (Nome) ≤ 200 characters
+    "nome" => "Nome Aqui" // [opcional] Nome do usuário pagador. Máximo: 25 caracteres.
   ],
   "valor" => [
-    "original" => "1.49" // [obrigatório/opcional] Valor original da cobrança.string \d{1,10}.\d{2} Obs: Na cobrança para QR Code dinâmico é obrigatório um valor, para estático não
+    "original" => "1.49" // [obrigatório] Valor original da cobrança.string \d{1,10}.\d{2} Obs: Para QR Code dinâmico, valor mínimo é de 0.01. Para QR Code poderá ser 0.00 (Ficará aberto para o pagador definir o valor)
   ],
   "chave" => $configuracoes["chave"], // [obrigatório] Determina a chave Pix registrada no DICT que será utilizada para a cobrança.
   "solicitacaoPagador" => "Serviço realizado.", // [opcional] determina um texto a ser apresentado ao pagador para que ele possa digitar uma informação correlata, em formato livre, a ser enviada ao recebedor.
-  "infoAdicionais" => [ // [opcional] Cada respectiva informação adicional contida na lista (nome e valor) deve ser apresentada ao pagador.
+  "infoAdicionais" => [ // [opcional] Cada respectiva informação adicional contida na lista (nome e valor) deve ser apresentada ao pagador. Campo presente somente no QR Code dinâmico
     [
       "nome" => "Campo 1", // Nome do campo string (Nome) ≤ 50 characters
       "valor" => "Informação Adicional1 do PSP-Recebedor" // Dados do campo string (Valor) ≤ 200 characters
@@ -65,18 +66,18 @@ $body = json_encode([
       "valor" => "Informação Adicional2 do PSP-Recebedor"
     ]
   ]
-]);
+];
 
 
 if ($tipo === "dinamico") { // Somente será gerado o payload para o QR Code dinâmico
-  $dadosPix = emitePix($pix_url_cob, $arq_certificado, $body, $tokenType, $accessToken);
+  $dadosPix = emitePix($pix_url_cob, $arq_certificado, json_encode($body), $tokenType, $accessToken);
 
   verficaFalhas($dadosPix); // Se encontrar falhas, apresentará a mensagem de erro e encerrará a execução
 } else { // Caso seja estático enviará as informações contidas no $body
-  $dadosPix = json_decode($body, true);
+  $dadosPix = $body;
   $dadosPix["txid"] = $txID;
 }
 
-$imagemQrCode = montaBrCode($dadosPix, $tipo, $pagoUmaVez, $cidade, $cep, $valorLivre, $tamanhoQrCode); // Monta o pix Copia e Cola e a imagem do QR Code
+$imagemQrCode = montaBrCode($dadosPix, $tipo, $pagoUmaVez, $nomeRecebedor, $cidade, $cep, $valorLivre, $tamanhoQrCode); // Monta o pix Copia e Cola e a imagem do QR Code
 
 echo $imagemQrCode;
--- montaBrCode.php
+++ montaBrCode.php
@@ -9,65 +9,59 @@
  * Visite o site da Gerencianet: https://www.gerencianet.com.br
 */
 
-function montaBrCode($dadosPix, $tipo, $pagoUmaVez, $cidade, $cep, $valorLivre, $tamanhoQrCode)
+function montaBrCode($dadosPix, $tipo, $pagoUmaVez, $nomeRecebedor, $cidade, $cep, $valorLivre, $tamanhoQrCode)
 {
 
-  $payload = getPayload($dadosPix["loc"]["location"]);
+  // Rotina montará a variável que correspondente ao payload no padrão EMV-QRCPS-MPM
+  $payload_format_indicator = '01';
+  $point_of_initiation_method = '12';
+  $merchant_account_information = '00' . preencheCampo('BR.GOV.BCB.PIX');
+  $merchant_category_code = '0000';
+  $transaction_currency = '986';
+  $country_code = 'BR';
 
-  if (!isset($payload["mensagem"])) { // Se o retorno da consulta do payload apresentar erro, não irá montar o QR Code e mostrará a mensagem
+  $payloadBrCode = "00" . preencheCampo($payload_format_indicator); // [obrigatório] Payload Format Indicator, valor fixo: 01
 
-    // Rotina montará a variável que correspondente ao payload no padrão EMV-QRCPS-MPM
+  if ($pagoUmaVez) { // Se o QR Code for para pagamento único (só puder ser utilizado uma vez), a variável $pagoUmaVez deverá ser true
+    $payloadBrCode .= "01" . preencheCampo($point_of_initiation_method); // [opcional] Point of Initiation Method Se o valor 12 estiver presente, significa que o BR Code só pode ser utilizado uma vez.
+  }
 
-    $payload_format_indicator = '01';
-    $point_of_initiation_method = '12';
-    $merchant_account_information = '00' . preencheCampo('BR.GOV.BCB.PIX');
-    $merchant_category_code = '0000';
-    $transaction_currency = '986';
-    $country_code = 'BR';
+  if ($tipo === "dinamico") {
+    $location = str_replace("https://", "", $dadosPix["loc"]["location"]); // [obrigatório] URL payload do PSP do recebedor que contém as informações da cobrança
+    $merchant_account_information .= '25' . preencheCampo($location);
+  } else { // Caso seja estático
+    $merchant_account_information .= '01' . preencheCampo($dadosPix["chave"]); //Chave do destinatário do pix, pode ser EVP, e-mail, CPF ou CNPJ.
+  }
+  $payloadBrCode .= '26' .  preencheCampo($merchant_account_information); // [obrigatório] Indica arranjo específico; “00” (GUI) e valor fixo: br.gov.bcb.pix
 
+  $payloadBrCode .= '52' . preencheCampo($merchant_category_code); // [obrigatório] Merchant Category Code “0000” ou MCC ISO18245
 
-    $payloadBrCode = "00" . preencheCampo($payload_format_indicator); // [obrigatório] Payload Format Indicator, valor fixo: 01
+  $payloadBrCode .= '53' . preencheCampo($transaction_currency); // [obrigatório] Moeda, “986” = BRL: real brasileiro - ISO4217
 
-    if ($pagoUmaVez) { // Se o QR Code for para pagamento único (só puder ser utilizado uma vez), a variável $pagoUmaVez deverá ser true
-      $payloadBrCode .= "01" . preencheCampo($point_of_initiation_method); // [opcional] Point of Initiation Method Se o valor 12 estiver presente, significa que o BR Code só pode ser utilizado uma vez.
-    }
+  $payloadBrCode .= '54';  // [opcional] Valor da transação. Utilizar o . como separador decimal.
+  $payloadBrCode .= ($valorLivre === true) ? preencheCampo('0.00') : preencheCampo($dadosPix["valor"]["original"]) ;
 
-    if ($tipo === "dinamico") {
-      $location = str_replace("https://", "", $dadosPix["loc"]["location"]); // URL payload do PSP do recebedor que contém as informações da cobrança
-      $merchant_account_information .= '25' . preencheCampo($location);
-    } else { // Caso seja estático
-      $merchant_account_information .= "01" . preencheCampo($dadosPix["chave"]); //Chave do destinatário do pix, pode ser EVP, e-mail, CPF ou CNPJ.
-    }
-    $payloadBrCode .= "26" .  preencheCampo($merchant_account_information); // [obrigatório] Indica arranjo específico; “00” (GUI) e valor fixo: br.gov.bcb.pix
+  $payloadBrCode .= '58' . preencheCampo($country_code); // [obrigatório] “BR” – Código de país ISO3166-1 alpha 2
 
-    $payloadBrCode .= '52' . preencheCampo($merchant_category_code); // [obrigatório] Merchant Category Code “0000” ou MCC ISO18245
+  $payloadBrCode .= '59';
+  $payloadBrCode .= preencheCampo($nomeRecebedor); // [obrigatório] Nome do beneficiário/recebedor. Máximo: 25 caracteres.
 
-    $payloadBrCode .= '53' . preencheCampo($transaction_currency); // [obrigatório] Moeda, “986” = BRL: real brasileiro - ISO4217
+  $payloadBrCode .= '60' . preencheCampo($cidade); // [obrigatório] Nome cidade onde é efetuada a transação. Máximo 15 caracteres.
 
-    $payloadBrCode .= '54';  // [opcional] Valor da transação. Utilizar o . como separador decimal.
-    $payloadBrCode .= ($valorLivre === true) ? preencheCampo('0') : ($tipo === "dinamico") ? preencheCampo($payload["valor"]["original"]) : preencheCampo($dadosPix["valor"]["original"]);
+  $payloadBrCode .= '61' . preencheCampo($cep); // [opcional] CEP da cidade onde é efetuada a transação.
 
-    $payloadBrCode .= '58' . preencheCampo($country_code); // [obrigatório] “BR” – Código de país ISO3166-1 alpha 2
-
-    $payloadBrCode .= '59'; // [opcional] Nome do beneficiário/recebedor. Máximo: 25 caracteres.
-    $payloadBrCode .= ($tipo === "dinamico") ? preencheCampo($payload["devedor"]["nome"]) : preencheCampo($dadosPix["devedor"]["nome"]); // [opcional] Nome do beneficiário/recebedor. Máximo: 25 caracteres.
-
-    $payloadBrCode .= '60' . preencheCampo($cidade); // [obrigatório] Nome cidade onde é efetuada a transação. Máximo 15 caracteres.
-
-    $payloadBrCode .= '61' . preencheCampo($cep); // [opcional] CEP da cidade onde é efetuada a transação.
-
-    $txID = ($tipo === "dinamico") ? $payload["txid"] : $dadosPix["txid"];
-    $aditional_data_field_template = '05' . preencheCampo($txID); // [opcional] Identificador da transação.
-    $payloadBrCode .= '62' . preencheCampo($aditional_data_field_template);
+  $txID = ($tipo === "dinamico") ? '***' : $dadosPix["txid"]; // [opcional] Identificador da transação.
+  $aditional_data_field_template = '05' . preencheCampo($txID); 
+  $payloadBrCode .= '62' . preencheCampo($aditional_data_field_template);
 
 
-    $payloadBrCode .= "6304"; // Adiciona o campo do CRC no fim da linha do pix.
+  $payloadBrCode .= "6304"; // Adiciona o campo do CRC no fim da linha do pix.
 
-    $payloadBrCode .= calculaChecksum($payloadBrCode); // Calcula o checksum CRC16 e acrescenta ao final.
+  $payloadBrCode .= calculaChecksum($payloadBrCode); // Calcula o checksum CRC16 e acrescenta ao final.
 
 
-    echo "Pix Copia e Cola: <input readonly='readonly' id='inputTest' value='$payloadBrCode' /> <button onclick='copiarTexto()''>Copiar</button> <br>";
-    echo "<script>
+  echo "Pix Copia e Cola: <input readonly='readonly' id='inputTest' value='$payloadBrCode' /> <button onclick='copiarTexto()''>Copiar</button> <br>";
+  echo "<script>
               let copiarTexto = () =>{
                   //captura o elemento input
                   const inputTest = document.querySelector('#inputTest');
@@ -80,11 +74,8 @@ function montaBrCode($dadosPix, $tipo, $pagoUmaVez, $cidade, $cep, $valorLivre, 
               };
           </script>";
 
-    $imageString = gerarQrCode($payloadBrCode, $tamanhoQrCode);
+  $imageString = gerarQrCode($payloadBrCode, $tamanhoQrCode);
 
-    // Exibe a imagem diretamente no navegador codificada em base64.
-    return '<img src="data:image/png;base64,' . $imageString . '"></center>';
-  } else {
-    return '<p>' . $payload["mensagem"] . '</p>';
-  }
+  // Exibe a imagem diretamente no navegador codificada em base64.
+  return '<img src="data:image/png;base64,' . $imageString . '"></center>';
 }
